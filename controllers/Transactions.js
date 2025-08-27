3134899f731a13c98ea21ffcb63f94844ce18b6f
Johan <johanelyosse@gmail.com>
2025-08-27 10:02:04 +0700

    push

--- controllers/Transactions.js
+++ controllers/Transactions.js
@@ -1,138 +1,51 @@
 const db = require('../models');
 const Transactions = db.Transactions;
 
-// Create and Save a new Transaction
-exports.create = (req, res) => {
-  // Validate request
-  if (!req.body.transaction_type) {
-    res.status(400).send({
-      message: "Content can not be empty!"
-    });
-    return;
+class Controller {
+  // LOGIN
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const merchant = await Merchants.findOne({ where: { email: email, deletedAt: null } });
+
+      if (!merchant) {
+        throw {
+          name: 'Merchant tidak ditemukan.',
+        };
+      }
+
+      if (!comparePassword(password, merchant.password)) {
+        throw { name: 'Email/Password Salah' };
+      }
+
+      const payload = {
+        merchant_id: merchant.merchant_id,
+        merchant_name: merchant.merchant_name,
+        email: merchant.email,
+        phone_number: merchant.phone_number,
+        address: merchant.address,
+        merchant_code: merchant.merchant_code,
+      };
+      const token = createAccessToken(payload);
+
+      res.status(201).json({
+        statusCode: 201,
+        message: 'Login successful.',
+        token: token,
+        data: {
+          merchant_id: merchant.merchant_id,
+          merchant_name: merchant.merchant_name,
+          merchant_code: merchant.merchant_code,
+          email: merchant.email,
+          phone_number: merchant.phone_number,
+          address: merchant.address,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
+}
 
-  // Create a Transaction
-  const transaction = {
-    transaction_type: req.body.transaction_type,
-    user_id: req.body.user_id,
-    amount: req.body.amount,
-    status: req.body.status,
-    timestamp: req.body.timestamp,
-    merchant_id: req.body.merchant_id,
-    sn_edc: req.body.sn_edc
-  };
-
-  // Save Transaction in the database
-  Transactions.create(transaction)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Transaction."
-      });
-    });
-};
-
-// Retrieve all Transactions from the database.
-exports.findAll = (req, res) => {
-  Transactions.findAll()
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving transactions."
-      });
-    });
-};
-
-// Find a single Transaction with an id
-exports.findOne = (req, res) => {
-  const transaction_id = req.params.transaction_id;
-
-  Transactions.findByPk(transaction_id)
-    .then(data => {
-      if (data) {
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Cannot find Transaction with id=${transaction_id}.`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving Transaction with id=" + transaction_id
-      });
-    });
-};
-
-// Update a Transaction by the id in the request
-exports.update = (req, res) => {
-  const transaction_id = req.params.transaction_id;
-
-  Transactions.update(req.body, {
-    where: { transaction_id: transaction_id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Transaction was updated successfully."
-        });
-      } else {
-        res.send({
-          message: `Cannot update Transaction with id=${transaction_id}. Maybe Transaction was not found or req.body is empty!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating Transaction with id=" + transaction_id
-      });
-    });
-};
-
-// Delete a Transaction with the specified id in the request
-exports.delete = (req, res) => {
-  const transaction_id = req.params.transaction_id;
-
-  Transactions.destroy({
-    where: { transaction_id: transaction_id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Transaction was deleted successfully!"
-        });
-      } else {
-        res.send({
-          message: `Cannot delete Transaction with id=${transaction_id}. Maybe Transaction was not found!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete Transaction with id=" + transaction_id
-      });
-    });
-};
-
-// Delete all Transactions from the database.
-exports.deleteAll = (req, res) => {
-  Transactions.destroy({
-    where: {},
-    truncate: false
-  })
-    .then(nums => {
-      res.send({ message: `${nums} Transactions were deleted successfully!` });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all transactions."
-      });
-    });
-};
+module.exports = Controller;
